0dcd0c7bb2288618d00fe7276fce6442d5addbea
Kevin Kemp <Kevin.Kemp@ActiveNetwork.com>
2013-07-27 19:21:04 -0500

    forgot to take this out when i deleted that view

--- LunchDeciderService/LunchDecider/app/lunchDecider.app.js
+++ LunchDeciderService/LunchDecider/app/lunchDecider.app.js
@@ -5,7 +5,6 @@ lunchDecider.config(['$routeProvider', '$locationProvider', function ($routeProv
     $routeProvider.
         when('/', { templateUrl: 'app/voteSessions.view.html', controller:'VoteSessionsController'}).
         when('/selectRestaurant', { templateUrl: 'app/selectRestaurant.view.html', controller: 'RestaurantController' }).
-        when('/restaurantDetails', { templateUrl: 'app/restaurantDetails.view.html' }).
         when('/voteSessionDetails', { templateUrl: 'app/voteSessionDetails.view.html', controller: 'RestaurantController' }).
         otherwise({ redirectTo: '/voteSessions' });
     $locationProvider.html5Mode(false);
